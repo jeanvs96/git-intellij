d792d77c4c51d734dcaabd730d3d40f480e52c43
Jean <jean.silva@dbccompany.com.br>
2022-06-09 20:26:10 -0300

    initial commit

---
+++ src/package1/TesteGit.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class TesteGit {
+    public static void main(String[] args) {
+        System.out.println("texto1");
+        System.out.println("texto2");
+        System.out.println("texto3");
+    }
+}
